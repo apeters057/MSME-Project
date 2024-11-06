c85d5ce72e0ed83ea40d7485fa1c8a8dbc33a829
apeters057 <99696011+apeters057@users.noreply.github.com>
2024-11-06 01:50:29 -0800

    Delete src/Earnings.py

--- src/Earnings.py
+++
@@ -1,9 +0,0 @@
-def calculate_earnings():
-    hourly_rate = float(input("Enter your hourly rate: $"))
-    hours_worked = float(input("Enter the number of hours worked: "))
-    
-    earnings = hourly_rate * hours_worked
-    return earnings
-
-# z = calculate_earnings()
-# print(z)
